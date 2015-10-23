5ab0a6229060b50e8952a32ebf583e6eac8d274b
Henri Watson <henri@henriwatson.com>
2015-10-23 12:02:05 +0100

    switch to using base64 encoding to send data to and receive data from the config panel.
    fixes #13.

--- LICENSE.md
+++ LICENSE.md
@@ -12,7 +12,7 @@ Skunk
 
 	The above copyright notice and this permission notice shall be included in all
 	copies or substantial portions of the Software.
-	
+
 	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -35,6 +35,31 @@ PebbleBucks was made available by Alexsander Akers under the [MIT License](http:
 	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 	copies of the Software, and to permit persons to whom the Software is
 	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+
+php.js
+======
+skunk makes use of the base64_encode and base64_decode functions made available by php.js under the [MIT License](http://opensource.org/licenses/MIT), reproduced below in its entirety:
+
+	Copyright (c) 2013 Kevin van Zonneveld (http://kvz.io)
+	and Contributors (http://phpjs.org/authors)
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy of
+	this software and associated documentation files (the "Software"), to deal in
+	the Software without restriction, including without limitation the rights to
+	use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+	of the Software, and to permit persons to whom the Software is furnished to do
+	so, subject to the following conditions:
 
 	The above copyright notice and this permission notice shall be included in all
 	copies or substantial portions of the Software.
--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -6,6 +6,81 @@ Skunk.token = '';
 Skunk.updating = false;
 Skunk.version = encodeURIComponent('1.2');
 
+// http://phpjs.org/functions/base64_encode/
+function base64_encode(data) {
+  var b64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+  var o1, o2, o3, h1, h2, h3, h4, bits, i = 0,
+    ac = 0,
+    enc = '',
+    tmp_arr = [];
+
+  if (!data) {
+    return data;
+  }
+
+  do { // pack three octets into four hexets
+    o1 = data.charCodeAt(i++);
+    o2 = data.charCodeAt(i++);
+    o3 = data.charCodeAt(i++);
+
+    bits = o1 << 16 | o2 << 8 | o3;
+
+    h1 = bits >> 18 & 0x3f;
+    h2 = bits >> 12 & 0x3f;
+    h3 = bits >> 6 & 0x3f;
+    h4 = bits & 0x3f;
+
+    // use hexets to index into b64, and append result to encoded string
+    tmp_arr[ac++] = b64.charAt(h1) + b64.charAt(h2) + b64.charAt(h3) + b64.charAt(h4);
+  } while (i < data.length);
+
+  enc = tmp_arr.join('');
+
+  var r = data.length % 3;
+
+  return (r ? enc.slice(0, r - 3) : enc) + '==='.slice(r || 3);
+}
+
+// http://phpjs.org/functions/base64_decode/
+function base64_decode(data) {
+  var b64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+  var o1, o2, o3, h1, h2, h3, h4, bits, i = 0,
+    ac = 0,
+    dec = '',
+    tmp_arr = [];
+
+  if (!data) {
+    return data;
+  }
+
+  data += '';
+
+  do { // unpack four hexets into three octets using index points in b64
+    h1 = b64.indexOf(data.charAt(i++));
+    h2 = b64.indexOf(data.charAt(i++));
+    h3 = b64.indexOf(data.charAt(i++));
+    h4 = b64.indexOf(data.charAt(i++));
+
+    bits = h1 << 18 | h2 << 12 | h3 << 6 | h4;
+
+    o1 = bits >> 16 & 0xff;
+    o2 = bits >> 8 & 0xff;
+    o3 = bits & 0xff;
+
+    if (h3 == 64) {
+      tmp_arr[ac++] = String.fromCharCode(o1);
+    } else if (h4 == 64) {
+      tmp_arr[ac++] = String.fromCharCode(o1, o2);
+    } else {
+      tmp_arr[ac++] = String.fromCharCode(o1, o2, o3);
+    }
+  } while (i < data.length);
+
+  dec = tmp_arr.join('');
+
+  return dec.replace(/\0+$/, '');
+}
+
 Skunk.loadState = function() {
   var state_json = window.localStorage.state;
   if (!state_json) return false;
@@ -25,16 +100,16 @@ Skunk.onShowConfiguration = function() {
   if (!window.localStorage.config || window.localStorage.config == "") {
     var url = Skunk.domain + '/settings';
   } else {
-    var url = Skunk.domain + '/settings#' + encodeURIComponent(window.localStorage.config);
+    var url = Skunk.domain + '/settings#' + base64_encode(window.localStorage.config);
   }
   Pebble.openURL(url);
 };
 
 Skunk.onWebViewClosed = function(event) {
   var response = event.response;
-  if (!response || decodeURIComponent(response).indexOf('{') === -1) return;
+  if (!response || base64_decode(response).indexOf('{') === -1) return;
 
-  window.localStorage.config = decodeURIComponent(response);
+  window.localStorage.config = base64_decode(response);
 
   Pebble.sendAppMessage({pushing_data: true});
 };
